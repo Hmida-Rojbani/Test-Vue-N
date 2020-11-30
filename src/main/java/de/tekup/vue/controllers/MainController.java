d49383be22f0779b136c3afe52fe70b1aa297ed4
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-11-30 20:09:03 +0100

    Add register success

--- src/main/java/de/tekup/vue/controllers/MainController.java
+++ src/main/java/de/tekup/vue/controllers/MainController.java
@@ -1,14 +1,27 @@
 package de.tekup.vue.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import de.tekup.vue.dto.models.User;
 
 @Controller
 public class MainController {
 	
 	@GetMapping("/register")
-	public String showForm() {
+	public String showForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
 		return "register_form";
+	}
+	
+	@PostMapping("/register")
+	public String submitForm(@ModelAttribute("user") User user) {
+		System.out.println(user);
+		return "register_success";
 	}
 
 }
